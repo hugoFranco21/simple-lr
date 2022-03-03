fdc0db7c417a2037a5d4c727ec36bed1aa1190a3
Hugo Franco <hfranco21@hotmail.com>
2022-03-03 12:12:28 -0600

    finish endpoint

--- predictions.py
+++ predictions.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import json
@@ -17,11 +15,11 @@ min_max_scaler.min_ = [-0.25, -0.25,  0.,    0.,    0.,    0.,    0.,    0.,    
 script_schema_1 = {
     "type": "object",
     "properties": {
-        "gender": {"type": "string"},
-        "education": {"type": "string"},
-        "job": {"type": "string"},
         "performance": {"type": "number"},
         "seniority": {"type": "number"},
+        "gender": {"type": "string"},
+        "education": {"type": "string"},
+        "job": {"type": "string"}     
     },
 }
 
@@ -54,7 +52,7 @@ def validateJson(jsonData, schema):
         return False
     return True
 
-def main(request):
+def get_prediction(input):
     """This function receives the data from the requests, parses it as a dictionary and gets the prediction
     Args:
         input (json) The request body
@@ -69,8 +67,65 @@ def main(request):
     if not (validateJson(data, script_schema_1)):
         return -1
     data = json.loads(input)
-    an_array = np.array(list(data.values()))
+    input_data = [data[0], data[1], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if data[2] == 'Female':
+        input_data[2] = 1
+    else:
+        input_data[3] = 1
+    
+    if data[3] == 'High School':
+        input_data[4] = 1
+    elif data[3] == 'College':
+        input_data[5] = 1
+    elif data[3] == 'Masters':
+        input_data[6] = 1
+    elif data[3] == 'PhD':
+        input_data[7] = 1
+        
+    if data[4] == 'Data Scientist':
+        input_data[8] = 1
+    elif data[4] == 'Driver':
+        input_data[9] = 1
+    elif data[4] == 'Financial Analyst':
+        input_data[10] = 1
+    elif data[4] == 'Graphic Designer':
+        input_data[11] = 1
+    elif data[4] == 'IT':
+        input_data[12] = 1
+    elif data[4] == 'Manager':
+        input_data[13] = 1
+    elif data[4] == 'Marketing Associate':
+        input_data[14] = 1
+    elif data[4] == 'Sales Associate':
+        input_data[15] = 1
+    elif data[4] == 'Software Engineer':
+        input_data[16] = 1
+    elif data[4] == 'Warehouse Associate':
+        input_data[17] = 1
+    an_array = np.array(list(input_data.values()))
     if not validate_input(an_array):
         return -1
-    pred = h(coefficients, input_data)
+    norm = min_max_scaler.transform([an_array])
+    pred = h(coefficients, norm)
     return pred
+
+def main(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    data = request.data
+    norm = get_prediction(data)
+    output = {
+        'success': False
+    }
+    if norm != -1:
+        output = {
+            'success': True,
+            'prediction': norm
+        }
+    return json.dumps(output)
